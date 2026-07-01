863ff5ff3012219f40412567d97cc9dbe3b91320
Nerd-Nikhil <nikhilkumarsingh1366@gmail.com>
2026-07-01 17:25:35 +0530

    admin update

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -38,6 +38,10 @@ const AdminDashboard = () => {
       }
     };
     fetchStats();
+    const intervalId = setInterval(fetchStats, 5000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   if (loading) {
