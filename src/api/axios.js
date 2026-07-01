7560331eac6e837e3d0324321a4a16a35c138878
Nerd-Nikhil <nikhilkumarsingh1366@gmail.com>
2026-07-01 11:21:35 +0530

    fixed all issues

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hr-management-backend-3uuw.onrender.com/",
+  baseURL: "https://hr-management-backend-3uuw.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
